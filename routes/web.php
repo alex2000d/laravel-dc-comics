2447c57ede0380553e7f98752436eb7779e19503
alex2000d <demarcoalessandro777@gmail.com>
2024-09-27 18:55:42 +0200

    [UPD] created the resources controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController as PageController;
+use App\Http\Controllers\ComicController as ComicController;
 
 /*
 |--------------------------------------------------------------------------
